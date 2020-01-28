28cc86a6bb43eb4600b64d3df0d4d4429d07b02b
Nikola Kunzová <nikola.kunzova@hotmail.sk>
2020-01-28 18:10:57 +0100

    Menu Finished (So far)

--- BachelorThesisImplementationV03/Assets/Scripts/Back.cs
+++ BachelorThesisImplementationV03/Assets/Scripts/Back.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GotoEditKnotBack : MonoBehaviour
+public class Back : MonoBehaviour
 {
     public string scene;
 
