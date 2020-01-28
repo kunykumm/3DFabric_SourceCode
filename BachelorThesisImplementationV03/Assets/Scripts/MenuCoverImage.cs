bcc5bcb711a5e66be345f42ff250eca8d59422a3
Nikola Kunzová <nikola.kunzova@hotmail.sk>
2020-01-28 17:47:30 +0100

    Intro Menu Started

---
+++ BachelorThesisImplementationV03/Assets/Scripts/MenuCoverImage.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuCoverImage : MonoBehaviour
+{
+    public Image coverImage;
+    public Text title;
+    public string nextScene;
+
+    private Color cover;
+    private Color text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cover = coverImage.color;
+        text = title.color;
+
+        cover.a = 0;
+        text.a = 0;
+
+        coverImage.color = cover;
+        title.color = text;
+    }
+
+    public void MouseHoversOver()
+    {
+        cover.a = 0.6f;
+        text.a = 1;
+
+        coverImage.color = cover;
+        title.color = text;
+    }
+
+    public void MouseIsOutside()
+    {
+        cover.a = 0;
+        text.a = 0;
+
+        coverImage.color = cover;
+        title.color = text;
+    }
+
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
