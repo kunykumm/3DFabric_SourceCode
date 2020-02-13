a726a0ad2ceb0d28a862cff5cf234b15004e5cfd
Nikola Kunzová <nikola.kunzova@hotmail.sk>
2020-02-13 11:54:22 +0100

    Knot Inheritance Introduced

---
+++ BachelorThesisImplementationV03/Assets/Scripts/KnotEditBase.cs
@@ -0,0 +1,66 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Dreamteck.Splines
+{
+    public class KnotEditBase : MonoBehaviour
+    {
+        public TubeGenerator tubeGenerator;
+        public SplineComputer splineComputer;
+
+        public Slider width;
+        public Slider detail;
+
+        public Text lineWidth;
+        public Text realWidth;
+        public Text realHeight;
+
+        public int rotationWhenGenerated;
+        public string sceneName;
+
+        protected float prevWidth;
+        protected float prevDetail;
+
+        private void Start()
+        {
+            PrepareValues();
+            ChangeWidth();
+        }
+
+        protected void PrepareValues()
+        {
+            PlayerPrefs.SetString("scene", sceneName);
+
+            prevWidth = width.value;
+            prevDetail = detail.value;
+
+            lineWidth.text = prevWidth.ToString("0.00");
+            realWidth.text = "0,00";
+            realHeight.text = "0,00";
+        }
+
+        public void OnEdit()
+        {
+            if (prevWidth != width.value) ChangeWidth();
+            if (prevDetail != detail.value) ChangeDetail();
+        }
+
+        protected void ChangeWidth()
+        {
+            prevWidth = width.value;
+            for (int i = 0; i < splineComputer.pointCount; ++i)
+            {
+                splineComputer.SetPointSize(i, prevWidth);
+            }
+            lineWidth.text = prevWidth.ToString("0.00");
+        }
+
+        protected void ChangeDetail()
+        {
+            prevDetail = detail.value;
+            tubeGenerator.sides = (int)prevDetail;
+        }
+    }
+}
---
+++ BachelorThesisImplementationV03/Assets/Scripts/KnotEditWithAngle.cs
@@ -0,0 +1,55 @@
+﻿using Dreamteck.Splines;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KnotEditWithAngle : KnotEditBase
+{
+    public Slider angle;
+
+    protected float prevAngle;
+
+    protected float xDiff;
+    protected float zDiff;
+    protected Vector3 firstPointStart;
+    protected Vector3 secondPointStart;
+
+    /**
+    * ANGLES
+    * Start position:   3.0  3.0  0.8
+    *                   0.0  3.0 -0.8
+    * End position:     1.5  3.0  1.6
+    *                   1.5  3.0 -1.6
+    *                   
+    * ALL NORMALS: At average center - best result when angle changes
+    */
+
+    private void Start()
+    {
+        PrepareValues();
+        prevAngle = angle.value;
+        CalculateBaseValues();
+        ChangeWidth();
+    }
+
+    protected void CalculateBaseValues()
+    {
+        firstPointStart = new Vector3(2.6f, 3.0f, 0.8f);
+        secondPointStart = new Vector3(0.4f, 3.0f, -0.8f);
+        xDiff = (firstPointStart.x - 1.5f) / angle.maxValue;
+        zDiff = (firstPointStart.z - 1.6f) / angle.maxValue;
+    }
+
+    public void OnEditWithAngle()
+    {
+        if (angle != null && prevAngle != angle.value) ChangeAngle();
+        OnEdit();
+    }
+
+    protected void ChangeAngle()
+    {
+        prevAngle = angle.value;
+        splineComputer.SetPointPosition(1, new Vector3(firstPointStart.x - prevAngle * xDiff, firstPointStart.y, firstPointStart.z - prevAngle * zDiff));
+        splineComputer.SetPointPosition(3, new Vector3(secondPointStart.x + prevAngle * xDiff, secondPointStart.y, secondPointStart.z + prevAngle * zDiff));
+    }
+
+}
