8dcb2ab25abdb1039140f09a4cbd1cbc1087c7fd
Nikola Kunzová <nikola.kunzova@hotmail.sk>
2020-02-13 22:53:09 +0100

    Info about Exported File

--- BachelorThesisImplementationV03/Assets/Scripts/ExportMesh.cs
+++ BachelorThesisImplementationV03/Assets/Scripts/ExportMesh.cs
@@ -1,23 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ExportMesh : MonoBehaviour
 {
     private GameObject[] theWholeMesh;
+    public Text savedInfo;
 
     public void ExportToBinarySTL()
     {
         theWholeMesh = GameObject.FindGameObjectsWithTag("knotrow");
-        string filePath = DefaultDirectory() + "/" + PlayerPrefs.GetString("scene") + "_net_bin_" + PlayerPrefs.GetInt("binary") + ".stl";
+        string fileName = PlayerPrefs.GetString("scene") + "_net_bin_" + PlayerPrefs.GetInt("binary") + ".stl";
+        string filePath = DefaultDirectory() + "/" + fileName;
         STL.Export(theWholeMesh, filePath);
+        savedInfo.text = "Your net was saved on Desktop as " + fileName + ".";
         PlayerPrefs.SetInt("binary", PlayerPrefs.GetInt("binary") + 1);
     }
     
     public void ExportToTextSTL()
     {
         theWholeMesh = GameObject.FindGameObjectsWithTag("knotrow");
-        string filePath = DefaultDirectory() + "/" + PlayerPrefs.GetString("scene") + "_net_text_" + PlayerPrefs.GetInt("text") + ".stl";
+        string fileName = PlayerPrefs.GetString("scene") + "_net_text_" + PlayerPrefs.GetInt("text") + ".stl";
+        string filePath = DefaultDirectory() + "/" + fileName;
         bool asASCII = true;
         STL.Export(theWholeMesh, filePath, asASCII);
+        savedInfo.text = "Your net was saved on Desktop as " + fileName + ".";
         PlayerPrefs.SetInt("text", PlayerPrefs.GetInt("text") + 1);
     }
 
